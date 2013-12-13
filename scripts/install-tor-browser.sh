c5d329f5d5893506e289745ec7eb78c6ca9bef77
Hannes Eichblatt <mail@hanneseichblatt.de>
2013-12-13 16:54:35 +0100

    TOR Browser install script now also creates .desktop
    I would have loved to do this as cp+sed, but this is the only way I
    got this to work.

--- scripts/install-tor-browser.sh
+++ scripts/install-tor-browser.sh
@@ -3,12 +3,14 @@
 # install TOR Browser Bundle for current user
 # $1 installation directory, defaults to ~/bin
 
+set -e
+
 URLPREFIX=https://www.torproject.org/download
 URLDOWNLOADPAGE=download-easy.html.en
 
 if [ "$#" == "0" ]; then
 	INSTALLDIR="$HOME"/bin
-    echo "No arguments provided, using default installation directory."
+    echo "No arguments provided."
 else
 	INSTALLDIR="$1"
 fi
@@ -21,5 +23,17 @@ URL="$URLPREFIX"/$(wget -q -O- "$URLPREFIX"/"$URLDOWNLOADPAGE" | grep "button li
 
 # download and untar the tarball
 wget -O- "$URL" | tar -xvz -C "$INSTALLDIR"
+
+# write .desktop file
+cat > "$HOME"/.local/share/applications/tor-browser.desktop <<END
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Tor-Browser
+Exec=${INSTALLDIR}/tor-browser_en-US/start-tor-browser
+Icon=${INSTALLDIR}/tor-browser_en-US/App/Firefox/icons/mozicon128.png
+Categories=Network;
+Comment=Browse the Web anonymously.
+END
 
 trap "" SIGHUP SIGINT SIGTERM
